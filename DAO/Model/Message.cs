222381adc68f9585d312287ffe6c4314bdec0ea0
Станислав <stas249501@gmail.com>
2015-09-17 00:54:34 +0300

    add model for messages

---
+++ DAO/Model/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO.Model
+{
+    public class Message
+    {
+        public long MessageId { get; set; }
+        public string Theme { get; set; }
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Body { get; set; }
+        public string FromId { get; set; }
+        public virtual User From { get; set; }
+        public string ToId { get; set; }
+        public virtual User To { get; set; }
+    }
+}
