b91e710de4331230ecb5bc9f71b1d6f029d0a480
Станислав <stas249501@gmail.com>
2016-01-17 22:22:08 +0200

    add nuget

--- EntityFrameworkDAO/PaymentDbInitializer.cs
+++ EntityFrameworkDAO/PaymentDbInitializer.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNet.Identity;
 
 namespace EntityFrameworkDAO
 {
-    class PaymentDbInitializer : DropCreateDatabaseIfModelChanges<PaymentDbContext>
+    class PaymentDbInitializer : DropCreateDatabaseAlways<PaymentDbContext>
     {
 
         protected override void Seed(PaymentDbContext context)
